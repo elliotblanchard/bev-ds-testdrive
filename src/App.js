b267581b7940cb2bc31007870d379d9114865d68
elliotblanchard <github@elliotblanchard.com>
2021-06-10 15:58:48 -0700

    Added some Bevy design elemenets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import ThemeProvider from './lib/theme/provider';
+import { Avatar, Checkbox, Chip, Logo } from '@bevy/bevy-design-system'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Some Bevy Design System Elements</h2>
+      <p>Avatar (base)</p>
+      <Avatar src='mock src' alt='mock alt' />
+      <p>Avatar (variant)</p>
+      <Avatar variant='square' src='mock src' alt='mock alt' />
+      <p>Checkbox</p>
+      <Checkbox label='Mock Label' value />
+      <p>Checkbox (variant)</p>
+      <Checkbox label='Mock Label' value={false} /> 
+      <p>Chip</p>
+      <Chip icon='' color='primary' label='Mock Label' variant='outlined' />     
+      <p>Chip (variant)</p>
+      <Chip icon='' color='secondary' label='Mock Label' variant='default' />             
+      <p>Logo</p>
+      <Logo />
     </div>
   );
 }
